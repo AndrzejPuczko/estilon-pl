7aedf4dcf743aa48935c2b059ce52a1377ec58b7
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-09-08 20:45:24 +0200

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Nav from './Nav'
+
+const Header = () => {
+	return (
+		<header className="flex fixed w-full bg-white border-b border-gray-300">
+			<Nav/>
+		</header>
+	)
+}
+
+export default Header
