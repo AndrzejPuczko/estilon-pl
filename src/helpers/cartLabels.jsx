a20258ee0b231e7dcaaaa97ecdbeb6ce469a7824
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-09-11 20:32:05 +0200

    feat: add labels for Cart component

---
+++ src/helpers/cartLabels.jsx
@@ -0,0 +1,24 @@
+const cartLabels = [
+	{
+		title: 'Packshoty',
+		description: 'test 2teta Lorem ipsum dolor sit amet consectetur. oaskoas oaksd oaks oasdkoa ksd okaso Lorem ipsum dolor sit amet. Lorem, ipsum.',
+		src: '/packshot.webp',
+	},
+	{
+		title: 'Animacje 360',
+		description: 'test 2teta Lorem ipsum dolor sit amet consectetur. oaskoas oaksd oaks oasdkoa ksd okaso Lorem ipsum dolor sit amet.',
+		src: '/360degree.webp',
+	},
+	{
+		title: 'Niewidzialny manekin',
+		description: 'test 2teta Lorem ipsum dolor sit amet consectetur. oaskoas oaksdLorem ipsum dolor sit amet.',
+		src: '/ghost.webp',
+	},
+	{
+		title: 'Projekty Indywidualne',
+		description: 'test 2teta Lorem ipsum dolor sit amet consectetur. oasko',
+		src: '/extra.webp',
+	},
+]
+
+export default cartLabels
