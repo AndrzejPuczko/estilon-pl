9d01ca93d38c39e2d3de696096008f731ec3f9cb
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-09-17 19:57:43 +0200

    fix: change heading and some styles

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,7 +5,8 @@ const AboutUs = () => {
 	return (
 		<div class="about-us bg-zinc-50 py-16" id="o-nas">
 			<div className="flex max-w-1400 my-0 mx-auto items-center flex-col text-center">
-				<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">O nas</h1>
+				<h3 className="scroll-m-20 pb-2 px-2 text-5xl font-bold tracking-tight transition-colors max-w-4xl leading-tight">O nas</h3>
+
 				<p className="leading-7 [&:not(:first-child)]:mt-6 max-w-5xl  px-10  text-gray-500">
 					<span className="text-lg font-semibold text-blue-500">Estilon</span> to ekspert w obszarze e-merchandisingu. Specjalizujemy się w fotografii produktowej, animacjach 360
 					oraz obsłudze kart produktowych - od optymalizacji po kompleksową kreację graficzną. Wyróżnia nas podejście konsultingowe - każda usługa Estilon obejmuje doradztwo
