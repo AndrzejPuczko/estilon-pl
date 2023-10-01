e1b7078d4deae1ea404a7c3835f0a9ec2c2c536b
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-10-01 20:21:52 +0200

    feat: add galleryItem helpers

---
+++ src/helpers/galleryItems.jsx
@@ -0,0 +1,56 @@
+const packshot = 'packshot'
+const animations = 'animations'
+
+const galleryItems = [
+	{
+		category: packshot,
+		src: '/homepage-gallery/1.jpg',
+		alt: 'Jakiś tytuł',
+		width: '1024',
+		height: '1024',
+		homepageGallery: true,
+	},
+	{
+		category: packshot,
+		src: '/homepage-gallery/3.jpg',
+		alt: 'Jakiś tytuł',
+		width: '1024',
+		height: '1024',
+		homepageGallery: true,
+	},
+	{
+		category: animations,
+		src: '/homepage-gallery/4.jpg',
+		alt: 'Jakiś tytuł',
+		width: '1024',
+		height: '1024',
+		homepageGallery: true,
+	},
+	{
+		category: animations,
+		src: '/homepage-gallery/5.jpg',
+		alt: 'Jakiś tytuł',
+		width: '1024',
+		height: '1024',
+		homepageGallery: true,
+	},
+	{
+		category: packshot,
+		src: '/homepage-gallery/2.jpg',
+		alt: 'Jakiś tytuł',
+		width: '1024',
+		height: '1024',
+		homepageGallery: true,
+	},
+	{
+		category: packshot,
+		src: '/homepage-gallery/6.jpg',
+		alt: 'Jakiś tytuł',
+		width: '1024',
+		height: '1024',
+		homepageGallery: true,
+	},
+	
+]
+
+export default galleryItems
