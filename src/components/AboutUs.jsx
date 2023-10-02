118880194efaca52183e391b70c7cb3f6a5b5e8f
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-10-02 20:32:48 +0200

    fix: change button variant and text color

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => {
 				<h3 className="scroll-m-20 pb-2 px-2 text-5xl font-bold tracking-tight transition-colors max-w-4xl leading-tight">O nas</h3>
 
 				<p className="leading-7 [&:not(:first-child)]:mt-6 max-w-5xl  px-10  text-gray-500">
-					<span className="text-lg font-semibold text-blue-500">Estilon</span> to ekspert w obszarze e-merchandisingu. Specjalizujemy się w fotografii produktowej, animacjach 360
+					<span className="text-lg font-semibold uppercase text-gray-800">Estilon</span> to ekspert w obszarze e-merchandisingu. Specjalizujemy się w fotografii produktowej, animacjach 360
 					oraz obsłudze kart produktowych - od optymalizacji po kompleksową kreację graficzną. Wyróżnia nas podejście konsultingowe - każda usługa Estilon obejmuje doradztwo
 					specjalistów.
 				</p>
@@ -16,7 +16,7 @@ const AboutUs = () => {
 					Dzielimy się wiedzą na temat najlepszych praktyk e-merchandisingu i wdrażamy je w projektach dla naszych klientów.
 				</p>
 				<p className="leading-7 max-w-5xl text-gray-500 px-10">Dzięki naszym inteligentnym e-wizualizacjom, sprzedaż w sieci jest skuteczna i zyskowna.</p>
-				<Button variant="red" className="mt-6">
+				<Button variant="destructive" className="mt-6">
 					Sprawdź
 				</Button>
 			</div>
@@ -26,3 +26,5 @@ const AboutUs = () => {
 }
 
 export default AboutUs
+
+
