2a6fd01c6c475f8c265d75de607ad367dd8a6feb
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-10-04 20:54:39 +0200

    feat: add offerContent helper

---
+++ src/helpers/offerContent.jsx
@@ -0,0 +1,41 @@
+export const OfferPackshot = () => {
+	return (
+		<>
+			<h2 className="mt-10 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight transition-colors">Fotografia produktowa</h2>
+			<p className="leading-7 text-sm mt-3">
+				Naszą główną specjalnością są zdjęcia produktowe czyli tzw. "packshoty". Produkt wzbudzający pożądanie zawsze bardzo dobrze się sprzedaje. Przygotowujemy zdjęcia produktowe
+				dostosowane do potrzeb e-handlu. Gwarantujemy ich wysoką jakość, szybką realizację projektów i uwzględnienie wymogów branżowych.
+			</p>
+			<h2 className="mt-10 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight transition-colors">Naszymi głównymi kategoriami fotografii są:</h2>
+			<ul className="leading-7 pt-3 text-sm">
+				<li>✔ packshoty</li>
+				<li>✔ manekin typu "duch"</li>
+				<li>✔ zrekonstruowany manekin</li>
+				<li>✔ fotografia reklamowa</li>
+				<li>✔ projekty ekstra</li>
+			</ul>
+		</>
+	)
+}
+
+export const OfferAnimations = () => {
+	return (
+		<>
+			<h2 className="mt-10 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight transition-colors">Animacje 360</h2>
+			<p className="leading-7 text-sm mt-3">
+				Wykonujemy animacje obrotowe 360 stopni, które pozwalają obejrzeć produkt z każdej strony - jak w klasycznym sklepie. To nowoczesne podejście, które pokazuje produkty w
+				przyjaznej i interaktywnej formie.
+			</p>
+			<h2 className="mt-10 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight transition-colors">Animacje 360 które wykonuje nasza firma, posiadają wiele zalet:</h2>
+			<ul className="leading-7 pt-3 text-sm">
+				<li>✔ dokładnie odzwierciedlają prezentowany produkt - klient zawsze wybiera ten sklep, w którym wyraźniej i nowocześniej jest przedstawiony interesujący go produkt</li>
+				<li>✔ powodują zwiększenie odwiedzin na stronie o nawet 50%</li>
+				<li>✔ sklep - strona nabiera większego zaufania poprzez profesjonalizm przedstawienia produktu</li>
+				<li>✔ przekazujemy pełne prawa autorskie co oznacza, że można wykorzystać zdjęcia z animacji w innych materiałach marketingowych takich jak banery, katalogi, broszury</li>
+				<li>✔ animacje 360 są nadal nowością na rynku, wiele stron internetowych nie korzysta z tego typu zdjęć</li>
+				<li>✔ animacje są responsywne, wyświetlają się zarówno na ekranach komputerów a także tabletów, smartfonów</li>
+				<li>✔ animacje są generowane w nowoczesnej technoligii HTML5 przez co są kompatybilne z 99% urządzeniami.</li>
+			</ul>
+		</>
+	)
+}
