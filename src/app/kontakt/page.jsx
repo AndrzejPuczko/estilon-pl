e4f69a2d49887855b959e6b185691104ad608d0f
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-10-10 20:00:55 +0200

    feat: add contact page

---
+++ src/app/kontakt/page.jsx
@@ -0,0 +1,14 @@
+import CategoryTitle from '@/components/CategoryTitle'
+import { ContactUs } from '@/components/ContactForm'
+
+const Contact = () => {
+	return (
+		<>
+			<CategoryTitle title="Kontakt" />
+			<ContactUs />
+         
+		</>
+	)
+}
+
+export default Contact
